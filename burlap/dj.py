5f5b47b503e8e6e163b535a07ddcf0c5990b81a2
Alex Ball <alex@ball.lol>
2019-10-21 21:32:26 +0000

    Speed up DjangoSatchel.get_media_timestamp()

--- burlap/dj.py
+++ burlap/dj.py
@@ -791,19 +791,18 @@ class DjangoSatchel(Satchel):
     @task
     def get_media_timestamp(self, last_timestamp=None):
         """
-        Retrieves the most recent timestamp of the media in the static root.
+        Retrieve the most recent timestamp of files in the static source paths.
 
-        If last_timestamp is given, retrieves the first timestamp more recent than this value.
+        If last_timestamp is given, retrieve the first timestamp more recent than this value.
         """
         r = self.local_renderer
-        _latest_timestamp = -1e9999999999999999
+        _latest_timestamp = float('-inf')
         for path in self.iter_static_paths():
             path = r.env.static_root + '/' + path
             self.vprint('checking timestamp of path:', path)
             if not os.path.isfile(path):
                 continue
-            #print('path:', path)
-            _latest_timestamp = max(_latest_timestamp, get_last_modified_timestamp(path) or _latest_timestamp)
+            _latest_timestamp = max(_latest_timestamp, os.path.getmtime(path) or _latest_timestamp)
             if last_timestamp is not None and _latest_timestamp > last_timestamp:
                 break
         self.vprint('latest_timestamp:', _latest_timestamp)
