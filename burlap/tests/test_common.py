5758de01f9c390c69f3d11a29c9839028111bfca
chrisspen@gmail.com <chrisspen@gmail.com>
2023-01-10 22:57:00 -0500

    Fixed tox config

--- burlap/tests/test_common.py
+++ burlap/tests/test_common.py
@@ -13,8 +13,6 @@ import unittest
 import getpass
 from pprint import pprint
 
-import six
-
 from fabric.api import runs_once
 
 from burlap import load_yaml_settings
@@ -25,12 +23,14 @@ from burlap.exceptions import SatchelDoesNotExist
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
+
 class _TestSatchel(Satchel):
 
     name = 'test'
 
     def configure(self):
         pass
+
 
 class CommonTests(TestCase):
 
@@ -82,6 +82,7 @@ class CommonTests(TestCase):
 
         s = "rsync --recursive --verbose --perms --times --links --compress --copy-links {exclude_str}  ' \
             '--delete --delete-before --force {rsync_auth} {rsync_source_dir} {rsync_target_host}{rsync_target_dir}"
+
         assert CMD_VAR_REGEX.findall(s) == ['exclude_str', 'rsync_auth', 'rsync_source_dir', 'rsync_target_host', 'rsync_target_dir']
 
     def test_settings_include(self):
@@ -95,19 +96,22 @@ class CommonTests(TestCase):
         except OSError:
             pass
 
-        open('/tmp/burlap_test/roles/all/settings.yaml', 'w').write("""
+        open('/tmp/burlap_test/roles/all/settings.yaml',
+             'w').write("""
 only_all_param: "just in all"
 overridden_by_prod: 123
 overridden_by_local: slkdjflsk
     """)
-        open('/tmp/burlap_test/roles/prod/settings.yaml', 'w').write("""inherits: all
+        open('/tmp/burlap_test/roles/prod/settings.yaml', 'w').write(
+            """inherits: all
 overridden_by_prod: 'prod'
 only_prod_param: 7891
 overriden_by_include: 7892
 overridden_by_local: oiuweoiruwo
 #includes: [settings_include1.yaml]
 includes: [settings_include2.yaml]
-    """)
+    """
+        )
         open('/tmp/burlap_test/roles/prod/settings_include2.yaml', 'w').write("""
 overriden_by_include: xyz
 overridden_by_local: ovmxlkfsweirwio
@@ -190,8 +194,13 @@ set_by_include3: 'some special setting'
 
         env0 = save_env()
         env.sites = {
-            'site1': {'apache_ssl': False, 'site': 'mysite'},
-            'site2': {'apache_ssl': True},
+            'site1': {
+                'apache_ssl': False,
+                'site': 'mysite'
+            },
+            'site2': {
+                'apache_ssl': True
+            },
         }
 
         lst = list(test.iter_sites())
@@ -203,13 +212,13 @@ set_by_include3: 'some special setting'
         assert len(lst) == 1
 
         # Confirm all non-default keys were removed.
-        assert set(_ for _ in env0 if not _.startswith('_')) == set(_ for _ in env if not _.startswith('_'))
+        assert {_ for _ in env0 if not _.startswith('_')} == {_ for _ in env if not _.startswith('_')}
 
         site_iter = test.iter_sites()
-        six.next(site_iter)
+        next(site_iter)
         print('env.SITE:', env.SITE)
         assert env.SITE == 'site1'
-        six.next(site_iter)
+        next(site_iter)
         print('env.SITE:', env.SITE)
         assert env.SITE == 'site2'
 
@@ -257,24 +266,30 @@ set_by_include3: 'some special setting'
 
         class ASatchel(Satchel):
             name = 'a'
+
             def set_defaults(self):
                 self.env.param = 123
+
             @task(precursors=['c'])
             def configure(self):
                 pass
 
         class BSatchel(Satchel):
             name = 'b'
+
             def set_defaults(self):
                 self.env.param = 123
+
             @task(precursors=['a', 'c'])
             def configure(self):
                 pass
 
         class CSatchel(Satchel):
             name = 'c'
+
             def set_defaults(self):
                 self.env.param = 123
+
             @task
             def configure(self):
                 pass
@@ -284,7 +299,7 @@ set_by_include3: 'some special setting'
         c_satchel = CSatchel()
         try:
 
-            assert set(all_satchels) == set(['A', 'B', 'C'])
+            assert set(all_satchels) == {'A', 'B', 'C'}
             deploy_satchel.genv.services = ['a', 'b', 'c']
 
             #assert init_plan_data_dir() == '.burlap/plans'
@@ -316,14 +331,69 @@ set_by_include3: 'some special setting'
         actual = sorted(all_satchels.keys())
         print('actual satchels:\n', actual)
         expected = [
-            'APACHE', 'AVAHI', 'BLUETOOTH', 'BUILDBOT', 'CELERY', 'CLOUDFRONT', 'CRON',
-            'DEBUG', 'DEPLOY', 'DEPLOYMENTNOTIFIER', 'DJ', 'DNS', 'EC2MONITOR', 'ELASTICSEARCH', 'FILE',
-            'GIT', 'GITCHECKER', 'GITTRACKER', 'GPSD', 'GROUP', 'HOST', 'HOSTNAME', 'HOSTSFILE',
-            'INADYN', 'IP', 'JIRAHELPER', 'JSHINT', 'LOCALES', 'LOGINNOTIFIER', 'MANIFEST', 'MONGODB', 'MOTION', 'MYSQL', 'MYSQLCLIENT', 'NM',
-            'NTPCLIENT', 'PACKAGER', 'PHANTOMJS', 'PIP', 'POSTFIX', 'POSTGRESQL', 'POSTGRESQLCLIENT', 'PROJECT',
-            'RABBITMQ', 'RDS', 'RPI', 'RSYNC', 'S3', 'SELENIUM', 'SERVICE', 'SNORT', 'SOFTWARERAID',
-            'SSHNICE', 'SSLHELPER', 'SUPERVISOR', 'TARBALL', 'TIMEZONE', 'UBUNTUMULTIVERSE',
-            'UNATTENDEDUPGRADES', 'USER', 'VAGRANT', 'VIRTUALBOX',
+            'APACHE',
+            'AVAHI',
+            'BLUETOOTH',
+            'BUILDBOT',
+            'CELERY',
+            'CLOUDFRONT',
+            'CRON',
+            'DEBUG',
+            'DEPLOY',
+            'DEPLOYMENTNOTIFIER',
+            'DJ',
+            'DNS',
+            'EC2MONITOR',
+            'ELASTICSEARCH',
+            'FILE',
+            'GIT',
+            'GITCHECKER',
+            'GITTRACKER',
+            'GPSD',
+            'GROUP',
+            'HOST',
+            'HOSTNAME',
+            'HOSTSFILE',
+            'INADYN',
+            'IP',
+            'JIRAHELPER',
+            'JSHINT',
+            'LOCALES',
+            'LOGINNOTIFIER',
+            'MANIFEST',
+            'MONGODB',
+            'MOTION',
+            'MYSQL',
+            'MYSQLCLIENT',
+            'NM',
+            'NTPCLIENT',
+            'OPENVPNCLIENT',
+            'PACKAGER',
+            'PHANTOMJS',
+            'PIP',
+            'POSTFIX',
+            'POSTGRESQL',
+            'POSTGRESQLCLIENT',
+            'PROJECT',
+            'RABBITMQ',
+            'RDS',
+            'RPI',
+            'RSYNC',
+            'S3',
+            'SELENIUM',
+            'SERVICE',
+            'SNORT',
+            'SOFTWARERAID',
+            'SSHNICE',
+            'SSLHELPER',
+            'SUPERVISOR',
+            'TARBALL',
+            'TIMEZONE',
+            'UBUNTUMULTIVERSE',
+            'UNATTENDEDUPGRADES',
+            'USER',
+            'VAGRANT',
+            'VIRTUALBOX',
         ]
         print('expected satchels:\n', expected)
         assert actual == expected
