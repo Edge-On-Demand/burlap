539ab82eae1db28b007cc036ba5216e8aafb97ed
chrisspen@gmail.com <chrisspen@gmail.com>
2023-08-08 19:12:15 -0400

    Modified @task to auto convert boolean flag arguments

--- burlap/decorators.py
+++ burlap/decorators.py
@@ -1,13 +1,11 @@
 """
 Convenience decorators for use in fabfiles.
 """
-#from Crypto import Random
 
-#from fabric import tasks
 from fabric.api import runs_once as _runs_once
 
-#from .context_managers import settings
 from burlap.tasks import WrappedCallableTask
+
 
 def task_or_dryrun(*args, **kwargs):
     """
@@ -33,32 +31,59 @@ def task_or_dryrun(*args, **kwargs):
 
     def wrapper(func):
         return task_class(func, *args, **kwargs)
+
     wrapper.is_task_or_dryrun = True
     wrapper.wrapped = func
 
     return wrapper if invoked else wrapper(func)
 
-_METHOD_ATTRIBUTES = ['deploy_before', 'is_post_callback']
 
-def _task(meth):
+_METHOD_ATTRIBUTES = ['deploy_before', 'is_post_callback', 'is_task']
+
+
+def kwargs_to_bool(kwargs, bools):
+    """
+    Converts all attributes beginning with "do_", or names in the bools list, to a boolean value.
+
+    Note, assumes boolean values are 1|0, as entered from a console as flags. This is not a direct wrapper around bool().
+
+    Arguments:
+
+        kwargs := {name1:value1, ...}
+
+        bools := {name1, name2, ...}
+    """
+    bools = bools or set()
+    for k, v in kwargs.items():
+        if k.startswith('do_') or k in bools:
+            try:
+                kwargs[k] = bool(int(v))
+            except ValueError as exc:
+                raise Exception(f'Unable to convert argument {k} with value "{v}" to a boolean.') from exc
+    return kwargs
+
+
+def _task(meth, bools):
     meth.is_task = True
 
     def wrapper(self, *args, **kwargs):
+        kwargs_to_bool(kwargs, bools)
         ret = meth(self, *args, **kwargs)
 
         # Ensure each satchels local variable scope is cleared after every server execution.
-        self.clear_local_renderer()
+        if hasattr(meth, 'is_deployer') or meth.__name__ == 'configure':
+            self.clear_local_renderer()
 
         return ret
 
-    if hasattr(meth, 'is_deployer') or meth.__name__ == 'configure':
-        # Copy the wrapped method's attributes to the wrapper.
-        wrapper.__name__ = meth.__name__
-        for attr in _METHOD_ATTRIBUTES:
-            if hasattr(meth, attr):
-                setattr(wrapper, attr, getattr(meth, attr))
-        return wrapper
-    return meth
+    # Copy the wrapped method's attributes to the wrapper so it's accessible from Fabric the same as the original method.
+    wrapper.__name__ = meth.__name__
+    for attr in _METHOD_ATTRIBUTES:
+        if hasattr(meth, attr):
+            setattr(wrapper, attr, getattr(meth, attr))
+
+    return wrapper
+
 
 def task(*args, **kwargs):
     """
@@ -74,23 +99,33 @@ def task(*args, **kwargs):
         def my_method(self):
             ...
 
+    Arguments:
+
+        precursors :=  List of other tasks to run before this one.
+
+        post_callback := True|False, If true, marks this as a function
+            to always be called during Burlap initialization.
+
+        bools := List of boolean argument names to auto-convert to boolean.
+
     """
     precursors = kwargs.pop('precursors', None)
     post_callback = kwargs.pop('post_callback', False)
+    bools = kwargs.pop('bools', [])
     if args and callable(args[0]):
         # direct decoration, @task
-        return _task(*args)
+        return _task(*args, bools)
 
     # callable decoration, @task(precursors=['satchel'])
     def wrapper(meth):
         if precursors:
             meth.deploy_before = list(precursors)
         if post_callback:
-            #from burlap.common import post_callbacks
-            #post_callbacks.append(meth)
             meth.is_post_callback = True
-        return _task(meth)
+        return _task(meth, bools)
+
     return wrapper
+
 
 def runs_once(meth):
     """
