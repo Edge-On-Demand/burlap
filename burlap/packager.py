a81db64e9cd12896adcd9ac1d70f8d241a85a9a1
Yamao Maoen <yamaomaoen@gmail.com>
2025-06-08 18:45:12 +0300

    Remove uninstall_blacklisted

--- burlap/packager.py
+++ burlap/packager.py
@@ -402,22 +402,6 @@ class PackagerSatchel(Satchel):
                 raise NotImplementedError
         return lst
 
-    @task
-    def uninstall_blacklisted(self):
-        """
-        Uninstalls all blacklisted packages.
-        """
-        from burlap.system import distrib_family
-        blacklisted_packages = self.env.blacklisted_packages
-        if not blacklisted_packages:
-            print('No blacklisted packages.')
-            return
-        family = distrib_family()
-        if family == DEBIAN:
-            self.sudo('DEBIAN_FRONTEND=noninteractive apt-get -yq purge %s' % ' '.join(blacklisted_packages))
-        else:
-            raise NotImplementedError('Unknown family: %s' % family)
-
     @task(precursors=['user', 'ubuntumultiverse', 'locales'])
     def configure(self, **kwargs):
         initial_upgrade = int(kwargs.pop('initial_upgrade', 1))
